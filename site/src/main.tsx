69fe20f32b3bf473d164792f01e7232998d8a278
Alejandro Espartal <alejandro.espartal@nunsys.com>
2025-04-21 13:20:11 +0200

    feat: add MUI theme provider and CssBaseline for consistent styling

--- site/src/main.tsx
+++ site/src/main.tsx
@@ -1,10 +1,32 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import { CssBaseline } from '@mui/material';
 import './index.css'
 import App from './App.tsx'
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1F2937',
+    },
+    secondary: {
+      main: '#3B82F6',
+    },
+    background: {
+      default: '#F9FAFB',
+    },
+    success:{
+      main: '#10B981',
+    }
+  },
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
